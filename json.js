2ca1e8c2719ccc80cbf2dccfed9d048d0e98948e
Valeria Zacarías <vall.zacarias26@gmail.com>
2023-05-16 12:12:56 -0300

    js json

---
+++ json.js
@@ -0,0 +1,228 @@
+export const insurance_plans = [
+    {
+        id: 1,
+        title: 'SNG02',
+        description: [
+            '100% cobertura en consultas ambulatorias',
+            'Ambulancia 24hs',
+            '50% descuento en medicamentos'
+        ],
+        price: 18000,
+        pictureUrl:
+            'https://imgur.com/a2B29lw'
+    },
+    {
+        id: 2,
+        title: 'SNG20',
+        description: [
+            '100% cobertura en consultas ambulatorias',
+            'Ambulancia 24hs',
+            '75% descuento en medicamentos',
+            '1 par de lentes gratis'
+        ],
+        price: 25000,
+        pictureUrl:
+            'https://imgur.com/OFwVVyP'
+    },
+    {
+        id: 3,
+        title: 'SNG40',
+        description: [
+            '100% cobertura en consultas ambulatorias',
+            'Ambulancia 24hs',
+            '75% descuento en medicamentos',
+            '1 par de lentes gratis',
+            'Consultas virtuales (Cam doctor)',
+            '1 cirugía gratis'
+        ],
+        price: 38000,
+        pictureUrl:
+            'https://imgur.com/AL4zfBw'
+    },
+    {
+        id: 4,
+        title: 'SNG50',
+        description: [
+            '100% cobertura en consultas ambulatorias',
+            'Ambulancia 24hs',
+            '75% descuento en medicamentos',
+            '1 par de lentes gratis',
+            'Consultas virtuales (Cam doctor)',
+            '1 cirugía gratis',
+            'Reintegro de $200.000 pesos por año en odontología'
+        ],
+        price: 60000,
+        pictureUrl:
+            'https://imgur.com/YTTy5Gf'
+    },
+    {
+        id: 5,
+        title: 'SNG80',
+        description: [
+            '100% cobertura en consultas ambulatorias',
+            'Ambulancia 24hs',
+            '90% descuento en medicamentos',
+            '1 par de lentes gratis',
+            'Consultas virtuales (Cam doctor)',
+            '2 cirugía gratis',
+            'Reintegro de $200.000 pesos por año en odontología',
+            '1 internación gratis'
+        ],
+        price: 80000,
+        pictureUrl:
+            'https://imgur.com/k4hxm0p'
+    },
+    {
+        id: 6,
+        title: 'SN100',
+        description: [
+            '100% cobertura en consultas ambulatorias',
+            'Ambulancia 24hs',
+            '90% descuento en medicamentos',
+            '1 par de lentes gratis',
+            'Consultas virtuales (Cam doctor)',
+            '2 cirugía gratis',
+            'Reintegro de $200.000 pesos por año en odontología',
+            '1 internación gratis',
+            'Cirugía de ojos gratis',
+            'Reintegro del 50% en gimnasio'
+        ],
+        price: 99.999,
+        pictureUrl:
+            'https://imgur.com/en75wYX'
+    }
+];
+
+export const devices = [
+    {
+        id: 1,
+        title: 'UD001',
+        description: [
+            'Modalidad Base Reclamo',
+            'Vigencia de 6 meses',
+            'Cobertura sobre hechos durante vigencia de la póliza',
+            'Cobertura en todos los centros médicos'
+        ],
+        price: 60000,
+        pictureUrl:
+            'https://imgur.com/3SSSAGW'
+    },
+    {
+        id: 2,
+        title: 'HO010',
+        description: [
+            'Atención domiciliaria para urgencias de plomería, electricidad, cerrajería y gas.',
+            'Traslado, guarda y custodia de muebles.',
+            'Servicio de urgencias y emergencias médicas.',
+            'Cobertura adicional para jugadores de golf'
+        ],
+        price: 182,
+        pictureUrl:
+            'https://imgur.com/DtYuqLC'
+    },
+    {
+        id: 3,
+        title: 'AU60',
+        description: [
+            'Seguro contra Todo Riesgo',
+            'Auxilio mecánico, remolque y asistencia.',
+            'Extensión de cobertura a países limítrofes.',
+            'Reintegro de gastos de hotelería y desplazamiento.'
+        ],
+        price: 182,
+        pictureUrl:
+            'https://imgur.com/hldhLT3'
+    },
+    {
+        id: 4,
+        title: 'SNG02',
+        description: [
+            '100% cobertura en consultas ambulatorias',
+            'Ambulancia 24hs',
+            '50% descuento en medicamentos'
+        ],
+        price: 18000,
+        pictureUrl:
+            'https://imgur.com/a2B29lw'
+    },
+    {
+        id: 5,
+        title: 'SNG20',
+        description: [
+            '100% cobertura en consultas ambulatorias',
+            'Ambulancia 24hs',
+            '75% descuento en medicamentos',
+            '1 par de lentes gratis'
+        ],
+        price: 25000,
+        pictureUrl:
+            'https://imgur.com/OFwVVyP'
+    },
+    {
+        id: 6,
+        title: 'SNG40',
+        description: [
+            '100% cobertura en consultas ambulatorias',
+            'Ambulancia 24hs',
+            '75% descuento en medicamentos',
+            '1 par de lentes gratis',
+            'Consultas virtuales (Cam doctor)',
+            '1 cirugía gratis'
+        ],
+        price: 38000,
+        pictureUrl:
+            'https://imgur.com/AL4zfBw'
+    },
+    {
+        id: 7,
+        title: 'SNG50',
+        description: [
+            '100% cobertura en consultas ambulatorias',
+            'Ambulancia 24hs',
+            '75% descuento en medicamentos',
+            '1 par de lentes gratis',
+            'Consultas virtuales (Cam doctor)',
+            '1 cirugía gratis',
+            'Reintegro de $200.000 pesos por año en odontología'
+        ],
+        price: 60000,
+        pictureUrl:
+            'https://imgur.com/YTTy5Gf'
+    },
+    {
+        id: 8,
+        title: 'SNG80',
+        description: [
+            '100% cobertura en consultas ambulatorias',
+            'Ambulancia 24hs',
+            '90% descuento en medicamentos',
+            '1 par de lentes gratis',
+            'Consultas virtuales (Cam doctor)',
+            '2 cirugía gratis',
+            'Reintegro de $200.000 pesos por año en odontología',
+            '1 internación gratis'
+        ],
+        price: 80000,
+        pictureUrl:
+            'https://imgur.com/k4hxm0p'
+    },
+    {
+        id: 9,
+        title: 'SN100',
+        description: [
+            '100% cobertura en consultas ambulatorias',
+            'Ambulancia 24hs',
+            '90% descuento en medicamentos',
+            '1 par de lentes gratis',
+            'Consultas virtuales (Cam doctor)',
+            '2 cirugía gratis',
+            'Reintegro de $200.000 pesos por año en odontología',
+            '1 internación gratis',
+            'Cirugía de ojos gratis',
+            'Reintegro del 50% en gimnasio'
+        ],
+        price: 99.999,
+        pictureUrl:
+            'https://imgur.com/en75wYX'
+    }
+];
